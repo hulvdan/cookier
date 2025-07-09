be83e70dd0d028f282ebfcd8d15175648da5947e
hulvdan <Hulvdan@yandex.ru>
2025-07-09 08:42:48 +0300

    Update bf_game.cpp

--- src/game/bf_game.cpp
+++ src/game/bf_game.cpp
@@ -1,13 +1,13 @@
 #pragma once
 
-UpdateFunctionResult GameUpdate() {
-  return UpdateFunctionResult_FINISHED_SUCCESSFULLY;
-}
-
 const char* GetWindowTitle() {
   return "The Game"
 #if BF_DEBUG
          " [DEBUG]"
 #endif
     ;
+}
+
+UpdateFunctionResult GameUpdate() {
+  return UpdateFunctionResult_FINISHED_SUCCESSFULLY;
 }
