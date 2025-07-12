a3bb03a394b2741ce03c74a0779d95e6346e4d6f
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-07-12 09:45:58 +0300

    Background image ratio fixed

--- src/engine/bf_game_exe.cpp
+++ src/engine/bf_game_exe.cpp
@@ -194,7 +194,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     // bgfx_reset( ge.meta.screenSize.x, ge.meta.screenSize.y, BGFX_RESET_VSYNC );
     bgfx::setDebug(BGFX_DEBUG_TEXT);
     // bgfx_set_view_clear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030FF, 1.0f, 0);
-    bgfx::setViewClear(0, BGFX_CLEAR_COLOR, 0x303030FF, 1.0f, 0);
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR, 0x600060FF, 1.0f, 0);
   }
 
   InitializeEngine();
@@ -206,6 +206,7 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
   bgfx::setViewRect(0, 0, 0, (u16)ge.meta.screenSize.x, (u16)ge.meta.screenSize.y);
   bgfx::touch(0);
 
+  EngineOnFrameStart();
   auto result = GameUpdate();
 
   if (result == SDL_APP_CONTINUE) {
