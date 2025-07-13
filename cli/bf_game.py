6bc34a0db61b5b96878fa27073b896568be69c34
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-07-13 19:23:12 +0300

    Example gamelib_processor

--- cli/bf_game.py
+++ cli/bf_game.py
@@ -1,7 +1,12 @@
 """
 USAGE:
 
-    from bf_lib import data_values
+    from bf_lib import data_values, gamelib_processor
 
     data_values.itch_target = "hulvdan/cult-boy"
+
+    @gamelib_processor
+    def process_tiles(_genline, gamelib) -> None:
+        for tile in gamelib["tiles"]:
+            tile.pop("codename")
 """
