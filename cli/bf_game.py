ca45a88d8e069e4dc545bff8fa8ed272fbba246e
hulvdan <Hulvdan@yandex.ru>
2025-07-27 14:57:41 +0300

    fix

--- cli/bf_game.py
+++ cli/bf_game.py
@@ -6,7 +6,7 @@ USAGE:
     data_values.itch_target = "hulvdan/cult-boy"
 
     @gamelib_processor
-    def process_tiles(_genline, gamelib) -> None:
+    def process_gamelib(_genline, gamelib) -> None:
         for tile in gamelib["tiles"]:
             tile.pop("codename")
 """
