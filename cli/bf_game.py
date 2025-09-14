fdc1fac37d1633e796db75f4a9a8f9141112dbc6
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-09-14 13:46:13 +0500

    working on "tree" mob

--- cli/bf_game.py
+++ cli/bf_game.py
@@ -14,7 +14,13 @@ USAGE:
 
 from itertools import groupby
 
-from bf_lib import data_values, gamelib_processor, genenum, recursive_replace_transform
+from bf_lib import (
+    SRC_DIR,
+    data_values,
+    gamelib_processor,
+    genenum,
+    recursive_replace_transform,
+)
 
 data_values.itch_target = "hulvdan/cookier"
 data_values.languages = ["russian", "english"]
@@ -32,17 +38,17 @@ def _process_gamelib(genline, gamelib, localization_codepoints: set[int]) -> Non
 
     # Items.
     # ============================================================
-    for x in gamelib["item__table"][1:]:
+    for x in gamelib["items"][1:]:
         x["name_locale"] = "ITEM_" + x["type"].upper()
 
     # Weapons.
     # ============================================================
-    for x in gamelib["weapon__table"][1:]:
+    for x in gamelib["weapons"][1:]:
         x["name_locale"] = "WEAPON_" + x["type"].upper()
 
     # Stats.
     # ============================================================
-    for i, x in enumerate(gamelib["stat__table"]):
+    for i, x in enumerate(gamelib["stats"]):
         if i >= 1:
             x["name_locale"] = "STAT_" + x["type"].upper()
         if i >= 2:
@@ -51,20 +57,40 @@ def _process_gamelib(genline, gamelib, localization_codepoints: set[int]) -> Non
     # Tables.
     # ============================================================
     if 1:
-        table_keys_to_update: list[str] = []
-        for key_ in gamelib:
-            suffix = "__table"
-            if key_.endswith(suffix):
-                key = key_.removesuffix(suffix)
-                table_keys_to_update.append(key)
-                types = [x.pop("type") for x in gamelib[key_]]
-                _check_duplicates(types)
-                genenum(genline, key.title() + "Type", types, add_count=True)
-                transforms.append(
-                    (f"{key}_type", f"{key}_types", {v: i for i, v in enumerate(types)})
+        with open(SRC_DIR / "game" / "bf_gamelib.fbs", encoding="utf-8") as in_file:
+            gamelib_fbs_lines = in_file.readlines()
+
+        start = -1
+        end = -1
+        for i, line in enumerate(gamelib_fbs_lines):
+            if "TABLE_GEN_START" in line:
+                start = i + 1
+            if "TABLE_GEN_END" in line:
+                end = i
+                break
+        assert start >= 0
+        assert end > start
+
+        # tuples of field_name + type_name
+        # ex: [("hostilities", "Hostility")]
+        table_transform_data: list[tuple[str, str]] = []
+        for i in range(start, end):
+            line = gamelib_fbs_lines[i]  # ex: `    hostilities: [Hostility] (required);`
+            field_name = line.split(":", 1)[0].strip()  # ex: `hostilities`
+            type_name = line.split("[", 1)[-1].split("]", 1)[0].strip()  # ex: `Hostility`
+            table_transform_data.append((field_name, type_name))
+
+        for field_name, type_name in table_transform_data:
+            types = [x.pop("type") for x in gamelib[field_name]]
+            _check_duplicates(types)
+            genenum(genline, type_name + "Type", types, add_count=True)
+            transforms.append(
+                (
+                    f"{type_name.lower()}_type",
+                    f"{type_name.lower()}_types",
+                    {v: i for i, v in enumerate(types)},
                 )
-        for key in table_keys_to_update:
-            gamelib[f"{key}s"] = gamelib.pop(f"{key}__table")
+            )
 
     # Normalization of wave creature spawn_factors.
     # ============================================================
