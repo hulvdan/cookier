f2b49391d60fb72aa1b160378858ffee045ec1d3
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-10-08 20:03:56 +0500

    fix gaps

--- src/game/bf_constants.cpp
+++ src/game/bf_constants.cpp
@@ -206,4 +206,15 @@ constexpr auto LANDMINE_DETONATION_FRAMES         = lframe::FromSeconds(0.5);
 PAL_COLORS_TABLE;
 #undef X
 
+Color TextifyColor(Color color) {  ///
+  auto v = ColorToHSV(color);
+  return ColorFromHSV(v.x, MIN(1, v.y * 1.2f), MIN(1, v.z * 1.2f));
+}
+
+#define X(name_, value_) const auto palText##name_ = TextifyColor(ColorFromRGBA(value_));
+// #define X(name_, value_) \
+//   constexpr auto palText##name_ = ColorContrast(ColorFromRGBA(value_), 0.5f);
+PAL_COLORS_TABLE;
+#undef X
+
 ///
