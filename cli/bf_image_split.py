a943957212871b3b5d099a80058c517f2d33a588
hulvdan <Hulvdan@yandex.ru>
2025-11-05 16:00:27 +0500

    wip игровое поле

--- cli/bf_image_split.py
+++ cli/bf_image_split.py
@@ -7,17 +7,31 @@ def _cv2pil(cv_img):
     return Image.fromarray(cv_img.astype("uint8"))
 
 
-def image_split(*, image: Image.Image) -> tuple[Image.Image, Image.Image]:
+def image_extract_white(image: Image.Image) -> Image.Image:
     img = np.asarray(image)
     h, w, _ = img.shape
-
     one = np.full((h, w), 255, np.uint8)
-
-    r_img = img[:, :, 0:1]
+    r_img = img[:, :, 0]
     alpha = img[:, :, 3]
     front_alpha = cv2.min(r_img, alpha)
-
     img_front = cv2.merge((one, one, one, front_alpha))
-    img_back = cv2.merge((one, one, one, alpha))
+    return _cv2pil(img_front)
 
-    return _cv2pil(img_front), _cv2pil(img_back)
+
+def image_extract_black(image: Image.Image) -> Image.Image:
+    img = np.asarray(image)
+    h, w, _ = img.shape
+    one = np.full((h, w), 255, np.uint8)
+    r_img = img[:, :, 0]
+    alpha = 255 - r_img
+    img_front = cv2.merge((one, one, one, alpha))
+    return _cv2pil(img_front)
+
+
+def image_extract_black_and_white(image: Image.Image) -> Image.Image:
+    img = np.asarray(image)
+    h, w, _ = img.shape
+    one = np.full((h, w), 255, np.uint8)
+    alpha = img[:, :, 3]
+    img_back = cv2.merge((one, one, one, alpha))
+    return _cv2pil(img_back)
