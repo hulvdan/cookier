910fd4d13e318e1c77df3e3e2a78f604d97ad9da
Aleksei Chistov <aleksey.chistov@gmail.com>
2025-12-02 22:20:28 +0500

    Update reaper_launch_codegen.lua

--- assets/sfx/reaper_launch_codegen.lua
+++ assets/sfx/reaper_launch_codegen.lua
@@ -1,1 +1,7 @@
-reaper.ExecProcess([[cmd /c "c:/Users/user/dev/home/cookier/cli/win_codegen_debug.bat"]], 0)
+local cmd = 'cmd.exe /C tasklist /FI "IMAGENAME eq game.exe"'
+local output = reaper.ExecProcess(cmd, 1000)  -- 1-second timeout
+
+-- Check if the process name appears in the result
+if output:match("game.exe") then
+  reaper.ExecProcess([[cmd /c "c:/Users/user/dev/home/cookier/cli/win_codegen_debug.bat"]], -1)
+end
