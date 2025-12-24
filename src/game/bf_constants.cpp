c8d29f5356746dd4443514a435872403594128a5
hulvdan <Hulvdan@yandex.ru>
2025-12-24 09:49:06 +0500

    fix

--- src/game/bf_constants.cpp
+++ src/game/bf_constants.cpp
@@ -63,7 +63,7 @@ constexpr f32 MELEE_WEAPON_COLLIDER_PHANTOM_RANGE_METERS = 0.12f;
 
 constexpr f32 WALKING_TUTORIAL_RADIUS_METERS = 4;
 
-constexpr f32 UI_ASPECT_RATIO_MIN = 4 / 3.0f;
+constexpr f32 UI_ASPECT_RATIO_MIN = 1172 / 1026.0f;  // From yandex screenshot.
 constexpr f32 UI_ASPECT_RATIO_MAX = 20 / 9.0f;
 
 // Other.
